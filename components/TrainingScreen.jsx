32d7181c036da10f9d64945d953819a928d1ebc7
ploesteanuDan <ploesteandan.99@gmail.com>
2020-11-13 19:52:54 +0200

    nuffin

--- components/TrainingScreen.jsx
+++ components/TrainingScreen.jsx
@@ -1,60 +1,14 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, ScrollView, FlatList, TouchableHighlight, Dimensions } from 'react-native'
-import {Spring} from 'react-spring/renderprops'
+import { Text, StyleSheet, View } from 'react-native'
 
-const db = {
-}
-
-export default class Trainings extends Component {
+export default class TrainingScreen extends Component {
     render() {
         return (
-            <View style={styles.trainingContainer}>
-               <Spring
-                    from={{
-                        opacity: 0,
-                        left: "-5%",
-                        height: "100%",
-                        left: "-5%",
-                        width: "100%",
-                        alignItems: 'center',
-                        justifyContent: 'center', 
-                    }}
-                    to={{ 
-                        opacity: 1,
-                        height: "100%",
-                        left: "0%",
-                        width: "100%",
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}
-                >
-                    {(mainSpring)=>(
-                        <View style={mainSpring}>
-                            {/* <TouchableHighlight>
-                                <View style={{
-                                    width: Dimensions.get("window").width * 0.97,
-                                    height: Dimensions.get("window").width * 0.97,
-                                    backgroundColor: "red"
-                                
-                                }}>
-                                    <Text>Hello</Text>
-                                </View>
-                            </TouchableHighlight> */}
-                        </View>
-                    )}
-                </Spring>
+            <View>
+                <Text> textInComponent </Text>
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({
-    trainingContainer: {
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: "dodgerblue",
-        width: "100%",
-        height: "100%",
-      },
-})
+const styles = StyleSheet.create({})
